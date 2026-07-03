28a9c3e87bd655d14bcfaadbf4e954e5be1d0c07
dlawnsdnjs <wns1628@gmail.com>
2026-07-03 15:19:12 +0900

    버킷 추가

--- src/main/java/com/example/community/filter/RateLimitFilter.java
+++ src/main/java/com/example/community/filter/RateLimitFilter.java
@@ -1,5 +1,7 @@
 package com.example.community.filter;
 
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.BucketConfiguration;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,9 +10,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class RateLimitFilter extends OncePerRequestFilter {
+    private static final BucketConfiguration configuration = BucketConfiguration.builder()
+            .addLimit(limit -> limit.capacity(10).refillGreedy(10, Duration.ofMinutes(1)))
+            .build();
+    // IP별로 Bucket 관리 (메모리 기반)
+    private final Map<String, Bucket> bucketCache = new ConcurrentHashMap<>();
     //RateLimit 적용 대상 API 목록
     private static final String[] PROTECTED_PATHS = {
       "/users/email"
@@ -21,9 +31,26 @@ public class RateLimitFilter extends OncePerRequestFilter {
         String requestURI = request.getRequestURI();
         if(shouldRateLimit(requestURI)){
             String ip = getIP(request);
-
+            Bucket bucket = bucketCache.computeIfAbsent(ip, key -> createBucket());
+            // tryConsume returns false immediately if no tokens available with the bucket
+            if (bucket.tryConsume(1)) {
+                // the limit is not exceeded
+                filterChain.doFilter(request, response);
+            } else {
+                // limit is exceeded=
+                response.setContentType("text/plain");
+                response.setStatus(429);
+                response.getWriter().append("Too many requests");
+            }
         }
     }
+
+    private Bucket createBucket() {
+        return Bucket.builder()
+                .addLimit(limit -> limit.capacity(10).refillGreedy(10, Duration.ofMinutes(1)))
+                .build();
+    }
+
 
     private boolean shouldRateLimit(String uri){
         for(String path : PROTECTED_PATHS){
