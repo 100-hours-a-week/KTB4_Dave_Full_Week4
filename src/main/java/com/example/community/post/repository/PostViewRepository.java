696948cfdbd236e87269600c6972a1779b6b08a3
dlawnsdnjs <wns1628@gmail.com>
2026-07-23 20:15:32 +0900

    Update PostViewRepository.java

--- src/main/java/com/example/community/post/repository/PostViewRepository.java
+++ src/main/java/com/example/community/post/repository/PostViewRepository.java
@@ -1,14 +1,29 @@
 package com.example.community.post.repository;
 
+import com.example.community.post.entity.Post;
 import com.example.community.post.entity.PostView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.Optional;
 
 @Repository
 public interface PostViewRepository extends JpaRepository<PostView, Long> {
     @EntityGraph(attributePaths = {"post"})
     Optional<PostView> findByPost_PostNumAndUserInfo_ProfileId(long postNum, long profileId);
+
+    @EntityGraph(attributePaths = {"post, userInfo"})
+    @Query("""
+        select pv.post
+        from PostView pv
+        where pv.viewAt >= :startTime
+        group by pv.post
+        order by count(pv) desc
+    """)
+    Page<Post> findPopularPost(Instant start, Pageable pageable);
 }
