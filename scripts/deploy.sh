98b7b30c4d9c2edb28ff2f81c3b1ea27b655b416
dlawnsdnjs <wns1628@gmail.com>
2026-07-31 09:12:39 +0900

    {} 중복 제거

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-ssh -i ~/.ssh/id_ed25519 -o StrictHostKeyChecking=no ${{ SERVER_USER }}@${{ SERVER_HOST }} << 'EOF'
+ssh -i ~/.ssh/id_ed25519 -o StrictHostKeyChecking=no ${ SERVER_USER }@${ SERVER_HOST } << 'EOF'
   cd ./community
   docker compose pull
   docker compose up -d
